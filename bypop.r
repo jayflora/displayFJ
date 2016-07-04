4cb64c6ab6bf7bed7ca0d3d367bc5d9f12258d86
jayflora <florajayf@gmail.com>
2016-07-04 18:23:30 +0200

    Update bypop.r
    adding comments

--- bypop.r
+++ bypop.r
@@ -1,7 +1,14 @@
+# adapted from OF note p.4
 
-
-bypop = function(coord,qmatrix,pop) {
-  # returns 2 matrix with rownames correponding to the populaation labels 
+bypop = function(qmatrix,coord,pop) {
+  # arguments:
+  # coord: matrix of coordinates  (nindividuals x 2)
+  # qmatrix: matrix of coefficients  (nindividuals x K) # K=number of clusters
+  # pop: generic vector of population labels, can be vector of numeric, character, factor
+  # returns:
+  # list of the 2 matrix qpop and coord.pop with values averaged over individuals of a same population 
+  # and with rownames correponding to the population labels 
+  
   ulabels=unique(pop)
   nlabels=length(ulabels)
   qpop = matrix(NA, ncol = ncol(qmatrix), nrow = nlabels)
@@ -12,17 +19,16 @@ bypop = function(coord,qmatrix,pop) {
   }
   rownames(qpop)=ulabels
   rownames(coord.pop)=ulabels
-  return( list(qpop,coord.pop) )
+  return( list(qpop=qpop,coord.pop=coord.pop) )
 }
 
-## EXAMPLE
-
-pop=rep(1:60, each = 10)
+## EXAMPLE (after loading everything as indicated in Olivier's note)
+pop=rep(paste("plant",1:60),each=10)
 tmp=bypop(coord,qmatrix,pop)
 qpop=tmp[[1]]
 coord.pop=tmp[[2]]
 
-pop=rep(paste("plant",1:60),each=10)
+pop=rep(1:60, each = 10)
 tmp=bypop(coord,qmatrix,pop)
 qpop=tmp[[1]]
 coord.pop=tmp[[2]]
